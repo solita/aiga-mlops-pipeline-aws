1dc8410a39619c224149ab222de8b9499c53f8a6
Joel Rontynen <joel.rontynen@kone.com>
2021-03-02 15:14:39 +0200

    Changed folder names.

--- preprocessing/sagemaker/resize_images.py
+++ preprocessing/sagemaker/resize_images.py
@@ -7,6 +7,9 @@ TARGET_IMAGE_SIZE = 416
 input_path = '/opt/ml/processing/input/images/'
 output_path = '/opt/ml/processing/output/resized_images/{}/images/'.format(TARGET_IMAGE_SIZE)
 
+if not os.path.exists(output_path):
+    os.makedirs(output_path)
+
 
 num_processed = 0
 image_names = os.listdir(input_path)
@@ -27,7 +30,7 @@ for name in image_names:
         if num_processed%50 == 0:
           print('Processed {} out of {} images'.format(num_processed, len(image_names)))
 
-    except:
-        print("Processing failed for:", name)
+    except OSError as e:
+        print("Processing failed for:", name + '.', e)
 
 print('Processed {} out of {} images'.format(num_processed, len(image_names)))
