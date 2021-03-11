909c0304c1148a60c9cf54b946cdbecdcf1e0e1a
Joel Rontynen <joel.rontynen@kone.com>
2021-03-11 11:38:53 +0200

    Reannotation.

--- preprocessing/sagemaker/reannotate_images.py
+++ preprocessing/sagemaker/reannotate_images.py
@@ -11,54 +11,29 @@ input_path = '/opt/ml/processing/input/'
 output_path = '/opt/ml/processing/output/resized_images/{}/annotations/'.format(TARGET_IMAGE_SIZE)
 
 
-
 columns = ['file_name', 'x1', 'y1', 'x2', 'y2', 'class', 'image_width', 'image_height']
-annotations = pd.read_csv(input_path + 'annotations.csv', names=columns)
-annotations = annotations[annotations['file_name'] == file_name]
-
 
 for group in ['train', 'val', 'test']:
-    with open("annotations_" + group + ".csv") as csvfile:
 
-        data = csv.DictReader(csvfile)
+    with open("annotations_" + group + ".csv", 'r') as csvfile:
+
+        data = csv.DictReader(csvfile, field_names=columns)
+
         for row in data:
 
-            row['image_height'] = row['image_height'] / TARGET_IMAGE_SIZE
-            row['image_width'] = row['image_width'] / TARGET_IMAGE_SIZE
+            x_center = row['x1'] + row['x2'] / 2.0
+            y_center = row['y1'] + row['y2'] / 2.0
+            obj_width = row['x2'] - row['x1']
+            obj_height = row['y2'] - row['y1']
 
-            row['x1'] = row['x1'] / row['image_width']
-            row['x2'] = row['x2'] / row['image_width']
-            row['y1'] = row['y1'] / row['image_height']
-            row['y2'] = row['y2'] / row['image_height']
+            x = x_center / row['image_width']
+            y = y_center / row['image_height']
+            w = obj_width / row['image_width']
+            h = obj_height / row['image_height']
 
+            output_row = f'0 {round(x, 8)} {round(y, 8)} {round(w, 8)} {round(h, 8)}\n'
 
+            image_name, _ = os.path.splitext(row['file_name'])
 
-new_height = annotations['image_height'].values[0] / TARGET_IMAGE_SIZE
-new_width = annotations['image_width'].values[0] / TARGET_IMAGE_SIZE
-
-for i in range(len(annotations)):
-
-    new_x1 = annotations['x1'].iloc[i] / new_width
-    new_x2 = annotations['x2'].iloc[i] / new_width
-
-    new_y1 = annotations['y1'].iloc[i] / new_height
-    new_y2 = annotations['y2'].iloc[i] / new_height
-
-    # object's width and height scaled to [0,1] relative to image size
-    obj_width = round((new_x2 - new_x1) / TARGET_IMAGE_SIZE, 8)
-    obj_height = round((new_y2 - new_y1) / TARGET_IMAGE_SIZE, 8)
-
-    # object's center coordinates scaled to [0,1] relative to image size
-    center_x = round(new_x1 / TARGET_IMAGE_SIZE + obj_width / 2, 8)
-    center_y = round(new_y1 / TARGET_IMAGE_SIZE + obj_height / 2, 8)
-
-    # create new record for object
-    obj = '0 {} {} {} {}\n'.format(center_x, center_y, obj_width, obj_height)
-
-    with open(output_path + 'annotations_' + os.path.splitext(file_name)[0] + '.txt', 'a') as f:
-        f.write(obj)
-
-
-
-
- 
+            with open(image_name + '.txt', 'a') as output_file:
+                output_file.write(output_row)
